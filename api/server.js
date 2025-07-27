e30e5bbb7c6e5ea51374bf59b623a013fc519a06
unknown <aadithsajeevan@gmail.com>
2025-07-27 14:11:43 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,57 @@
+import express, { request } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import jwt from "jsonwebtoken"
+dotenv.config();
+const app = express();
+
+// const token=jwt.sign({id: user._id,isSeller: user.isSeller,},process.env.JWT_SECRET)
+// const token = jwt.sign(
+//   { id:user._id },
+//   process.env.JWT_SECRET, // This must not be undefined
+//   { expiresIn:"5d"}
+// );
+
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Backend server is running!");
+});
